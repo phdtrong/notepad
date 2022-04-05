467d6ad9a7a5749ec89567e4ac498dd66625b06a
areshamilton <46938122+areshamilton@users.noreply.github.com>
2022-04-04 21:38:39 -0700

    Create changelog.cpp

---
+++ changelog.cpp
@@ -0,0 +1,2 @@
+// TODO: changelog should be able to both read from and write to a text file called "Change Log", that holds every update to the user's notes
+//       i.e. writing a new note, deleting a note, etc.
