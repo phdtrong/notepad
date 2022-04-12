612cbcf7acad85a588e138d7cef8ad1dc2e271b1
areshamilton <areshamilton@csu.fullerton.edu>
2022-04-11 20:16:10 -0700

    made Entry class and basic menu

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
-#include <iostream>
-
 // TODO: main should start the program and hold the menu
+#include "changelog.cpp"
+#include "notes.cpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+  std::string selection;
+  std::vector<Entry> notepad;
+
+  std::cout << "Notepad Program\n(type in a number to select an option)\n";
+
+  // this loop keeps the menu up until the user exits the program
+  do
+  {
+    std::cout << "1: Make a new entry\n"
+              << "2: View notepad\n"
+              << "3: Save notepad\n"
+              << "4: Import notepad file\n"
+              << "0: Close program\n";
+
+    std::cin >> selection;
+  } while (selection != "0");
+
+  return 0;
+}
--- notes.cpp
+++ notes.cpp
@@ -1,1 +1,37 @@
 // TODO: implement classes and methods for notes (ability to write notes, delete notes, sort them, etc.)
+#include <string>
+#include <vector>
+
+// this class pertains to each entry a user would create in the notepad
+class Entry
+{
+
+private:
+  std::string _header, _body;
+
+public:
+
+  // getters and setters for each attribute
+  std::string get_header()
+  {
+    return _header;
+  }
+
+  std::string get_body()
+  {
+    return _body;
+  }
+
+  void set_header(std::string new_header)
+  {
+    _header = new_header;
+  }
+
+  void set_body(std::string new_body){
+    _body = new_body;
+  }
+
+
+  // constructor
+  Entry(std::string header, std::string body) : _header(header), _body(body) {};
+};
